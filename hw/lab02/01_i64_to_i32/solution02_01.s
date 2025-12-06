ee2144b81702ccc1ae9261b57ce65998ff95862a
agetosha <mahlachev44@gmail.com>
2025-12-06 17:53:29 +0800

    Implement i64 to i32 conversion with bounds checking

--- hw/lab02/01_i64_to_i32/solution02_01.s
+++ hw/lab02/01_i64_to_i32/solution02_01.s
@@ -1,3 +1,29 @@
 .intel_syntax noprefix
-
 .global main
+
+main:
+    call readi64
+
+    mov rbx, 2147483647
+    cmp rax, rbx
+    jg .set_max
+
+    mov rbx, -2147483648
+    cmp rax, rbx
+    jl .set_min
+
+    mov rdi, rax
+    jmp .print_result
+
+.set_max:
+    mov rdi, 2147483647
+    jmp .print_result
+
+.set_min:
+    mov rdi, -2147483648
+    jmp .print_result
+
+.print_result:
+    call writei32
+    call nl
+    call finish
