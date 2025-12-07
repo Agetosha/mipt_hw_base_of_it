fe329064de9c98cc59a028e535114a984126d898
Agetosha <mahlachev44@gmail.com>
2025-12-07 23:18:14 +0800

    Done 02_02

--- hw/lab02/02_countdown/solution02_02.s
+++ hw/lab02/02_countdown/solution02_02.s
@@ -1,4 +1,25 @@
 .intel_syntax noprefix
-.text
-.globl  main
+.global main
 
+.text
+main:
+    push rbp
+    mov rbp, rsp
+    
+    call readi32
+    mov r12d, eax
+
+    cmp r12d, 0
+    jl .done
+
+.my_loop:
+    mov edi, r12d
+    call writei32
+    call nl
+
+    dec r12d
+    cmp r12d, 0
+    jge .my_loop
+
+.done:
+    call finish
